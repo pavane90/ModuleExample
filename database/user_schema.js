0cc5e34f5921530942bbfa6a46ce90f3e647eaf5
Lee, Byungman <pavane90@gmail.com>
2019-07-29 16:34:21 +0900

    fix userschema

--- database/user_schema.js
+++ database/user_schema.js
@@ -1,17 +1,60 @@
-UserSchema = mongoose.Schema({
-  id: { type: String, required: true, unique: true, default: "" },
-  hashed_password: { type: String, required: true, default: "" },
-  salt: { type: String, required: true },
-  name: { type: String, index: "hashed", default: "" },
-  age: { type: Number, default: -1 },
-  created_at: { type: Date, index: { unique: false }, default: Date.now() },
-  updated_at: { type: Date, index: { unique: false }, default: Date.now() }
-}); //user 테이블 정의
-console.log("UserSchema 정의함");
-UserSchema.static("findById", function(id, callback) {
-  return this.find({ id: id }, callback);
-});
+var Schema = {};
 
-UserSchema.static("findAll", function(callback) {
-  return this.find({}, callback);
-});
+Schema.createSchema = function() {
+  UserSchema = mongoose.Schema({
+    id: { type: String, required: true, unique: true, default: "" },
+    hashed_password: { type: String, required: true, default: "" },
+    salt: { type: String, required: true },
+    name: { type: String, index: "hashed", default: "" },
+    age: { type: Number, default: -1 },
+    created_at: { type: Date, index: { unique: false }, default: Date.now() },
+    updated_at: { type: Date, index: { unique: false }, default: Date.now() }
+  }); //user 테이블 정의
+  console.log("UserSchema 정의함");
+
+  UserSchema.virtual("password").set(function(password) {
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+    console.log("virtual password 저장됨 : " + this.hashed_password);
+  });
+
+  UserSchema.method("encryptPassword", function(plainText, inSalt) {
+    if (inSalt) {
+      return crypto
+        .createHmac("sha1", inSalt)
+        .update(plainText)
+        .digest("hex");
+    } else {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(plainText)
+        .digest("hex");
+    }
+  });
+
+  UserSchema.method("makeSalt", function() {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  });
+
+  UserSchema.method("authenticate", function(
+    plainText,
+    inSalt,
+    hashed_password
+  ) {
+    if (inSalt) {
+      console.log("authenticate 호출됨");
+      return this.encryptPassword(plainText, inSalt) === hashed_password;
+    } else {
+      console.log("authenticate 호출됨");
+      return this.encryptPassword(plainText) === hashed_password;
+    }
+  });
+
+  UserSchema.static("findById", function(id, callback) {
+    return this.find({ id: id }, callback);
+  });
+
+  UserSchema.static("findAll", function(callback) {
+    return this.find({}, callback);
+  });
+};
