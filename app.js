a8bb895ae0bf748eb4ed9497c63bb30c16a5e48b
Lee, Byungman <pavane90@gmail.com>
2019-07-30 15:34:28 +0900

    require user

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var expressSession = require("express-session");
 
 //에러 핸들러
 var expressErrorHandler = require("express-error-handler");
+
+var user = require("./routes/user");
+
 //암호화 모듈
 var crypto = require("crypto");
 //mongoose 사용
@@ -69,127 +72,11 @@ app.use(
 
 var router = express.Router();
 
-router.route("/process/login").post(function(req, res) {
-  console.log("/process/login 라우팅 함수 호출됨.");
+router.route("/process/login").post(user.login);
 
-  var paramId = req.body.id || req.query.id;
-  var paramPassword = req.body.password || req.query.password;
-  console.log("요청 파라미터 : " + paramId + ", " + paramPassword);
+router.route("/process/adduser").post(user.adduser);
 
-  if (database) {
-    authUser(database, paramId, paramPassword, function(err, docs) {
-      if (err) {
-        console.log("에러가 발생했습니다" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>에러 발송</h1>");
-        res.end();
-        return;
-      }
-      if (docs) {
-        console.dir(docs);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>로그인 성공!</h1>");
-        res.write("<div><p>사용자 : " + docs[0].name + "</p></div>");
-        res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
-        res.end();
-      } else {
-        console.log("대상 데이터가 없음" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>존재하지 않는 사용자입니다.</h1>");
-        res.end();
-      }
-    });
-  } else {
-    console.log("데이터베이스 연결에러" + err);
-    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-    res.write("<h1>데이터베이스 연결에러</h1>");
-    res.end();
-  }
-});
-
-router.route("/process/adduser").post(function(req, res) {
-  console.log("/process/adduser 라우팅 호출");
-
-  var paramId = req.body.id || req.query.id;
-  var paramPassword = req.body.password || req.query.password;
-  var paramName = req.body.name || req.query.name;
-
-  console.log(
-    "요청 파라미터 : " + paramId + ", " + paramPassword + ", " + paramName
-  );
-
-  if (database) {
-    addUser(database, paramId, paramPassword, paramName, function(err, result) {
-      if (err) {
-        console.log("에러가 발생했습니다" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>에러 발송</h1>");
-        res.end();
-        return;
-      }
-      if (result) {
-        console.dir(result);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>등록 완료!</h1>");
-        res.write("<div><p>사용자 : " + paramName + "</p></div>");
-        res.end();
-      } else {
-        console.log("에러 발생" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>사용자 추가에 실패했습니다.</h1>");
-        res.end();
-      }
-    });
-  } else {
-    console.log("데이터베이스 연결에러" + err);
-    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-    res.write("<h1>데이터베이스 연결에러</h1>");
-    res.end();
-  }
-});
-
-router.route("/process/listuser").post(function(req, res) {
-  console.log("/process/listuser 라우팅 함수 호출됨");
-  // 사용자 리스트 전체 출력
-  if (database) {
-    UserModel.findAll(function(err, results) {
-      //해당 스키마의 내용 전부 가져오기
-      if (err) {
-        console.log("에러가 발생했습니다" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>에러 발송</h1>");
-        res.end();
-        return;
-      }
-      if (results) {
-        console.dir(results);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h3>사용자 리스트</h3>");
-        res.write("<div><ul>");
-
-        for (var i = 0; i < results.length; i++) {
-          var curId = results[i]._doc.id;
-          var curName = results[i]._doc.name;
-          res.write(
-            "    <li>#" + (i + 1) + "->" + curId + ", " + curName + "</li>"
-          );
-        }
-        res.write("</ul></div>");
-        res.end();
-      } else {
-        console.log("에러 발생" + err);
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-        res.write("<h1>조회된 사용자 없음</h1>");
-        res.end();
-      }
-    });
-  } else {
-    console.log("데이터베이스 연결에러" + err);
-    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-    res.write("<h1>데이터베이스 연결에러</h1>");
-    res.end();
-  }
-});
+router.route("/process/listuser").post(user.listuser);
 
 app.use("/", router);
 
